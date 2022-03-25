c81c42cd1a33e098f203253722aa153fba9becb5
Jonathan Curtis <06jcurtis@gmail.com>
2022-03-25 11:01:46 -0400

    added basic mechanics

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Board from "./tetris/Board";
+
+function Game() {
+  return (
+    <div className="t-parent">
+      <Board />
+    </div>
+  );
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
